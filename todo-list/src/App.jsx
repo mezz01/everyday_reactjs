523c5b5701d471ffd2fa1114266c29651de864cb
Amine <mezzatamine2002@gmail.com>
2023-11-08 01:52:23 +0100

    to do logic with useState

--- todo-list/src/App.jsx
+++ todo-list/src/App.jsx
@@ -4,23 +4,51 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
+  const [newTodo, setNewTodo] = useState("")
+  const [todo, setTodo] = useState([])
+
+  function handleTodo(e) {
+    // preventing react from refreshing website
+    e.preventDefault()
+
+    setTodo(currentTodo =>{
+      return[
+        ...currentTodo,
+        {id:crypto.randomUUID(), title: newTodo, completed: false},
+      ]
+    })
+
+   setNewTodo("")
+  }
   return (
     <>
       <h1>My To Do List</h1>
-      <form className='new-todo-form'>
+      <form onSubmit={handleTodo} className='new-todo-form'>
         <div className='row-todo'>
           <label htmlFor='item'>Add the thing (we know you won't do)</label>
-          <input type='text' id='item'/>
+          <input 
+          value={newTodo}
+          onChange={e => setNewTodo(e.target.value)}
+          type='text' 
+          id='item'
+          />
         </div>
         <button className='btn-add'>Add to list</button>
       </form>
       <h1>My todays list</h1>
       <ul>
-        <li className='h-10'>torororo my to do that m not doing</li>
-        <li>torororo my to do that m not doing</li>
-        <li>torororo my to do that m not doing</li>
-        <li>torororo my to do that m not doing</li>
-        <li>torororo my to do that m not doing</li>
+        <li>
+          <label>
+            <input type='checkbox' />
+            Todo 1
+          </label>
+        </li>
+        <li>
+          <label>
+            <input type='checkbox' />
+            Todo 2
+          </label>
+        </li>
       </ul>
     </>
   )
